4fe640799d574855fa238b1becaa834605d8f2f4
Bosco Domingo <boscodomingob@gmail.com>
2023-04-16 20:58:31 +0200

    Improve BaseController

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,17 +1,29 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import BaseService from '../services/base.service';
 
 export default class BaseController {
   constructor(private readonly baseService: BaseService) {}
 
-  public getSampleData(req: Request, res: Response): void {
-    const sampleData = this.baseService.getSampleData();
-    res.json(sampleData);
+  public getSampleData(req: Request, res: Response, next: NextFunction): void {
+    try {
+      const sampleData = this.baseService.getSampleData();
+      res.json(sampleData);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public postReceivedData(req: Request, res: Response): void {
-    const data = JSON.stringify(req.body, null, 2);
-    res.json({ message: `Received data: ${data}` });
+  public postReceivedData(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): void {
+    try {
+      const data = JSON.stringify(req.body, null, 2);
+      res.json({ message: `Message received`, data });
+    } catch (error) {
+      next(error);
+    }
   }
 }
